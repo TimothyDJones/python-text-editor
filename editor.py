8a84037f19ade636d888259b5691c43c2be17c64
Tim Jones <tdjones74021@yahoo.com>
2020-12-30 11:08:41 -0600

    Add capability to load file by passing filename at command line.

--- editor.py
+++ editor.py
@@ -2,14 +2,26 @@
 # A simple text editor built with Python curses standard library.
 # https://wasimlorgat.com/editor
 
+import argparse
 import curses
 import sys
 
 def main(stdscr):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    
+    with open(args.filename) as f:
+        buffer = f.readlines()
+    
     while True:
+        stdscr.erase()
+        for row, line in enumerate(buffer):
+            stdscr.addstr(row, 0, line)
+
         k = stdscr.getkey()
         if k == "q":
-            sys.exit()
+            sys.exit(0)
         
 if __name__ == "__main__":
     curses.wrapper(main)
