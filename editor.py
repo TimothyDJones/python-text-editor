0c3cd1dc6ee6e24c2566eef893e7410e48d6b71b
Tim Jones <tdjones74021@yahoo.com>
2020-12-30 11:19:49 -0600

    Add 'Window' class to size editor buffer to fit screen.

--- editor.py
+++ editor.py
@@ -6,6 +6,11 @@ import argparse
 import curses
 import sys
 
+class Window:
+    def __init__(self, n_rows, n_cols):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+
 def main(stdscr):
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
@@ -14,10 +19,12 @@ def main(stdscr):
     with open(args.filename) as f:
         buffer = f.readlines()
     
+    window = Window(curses.LINES - 1, curses.COLS - 1)
+    
     while True:
         stdscr.erase()
-        for row, line in enumerate(buffer):
-            stdscr.addstr(row, 0, line)
+        for row, line in enumerate(buffer[:window.n_rows]):
+            stdscr.addstr(row, 0, line[:window.n_cols])
 
         k = stdscr.getkey()
         if k == "q":
