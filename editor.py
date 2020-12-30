f92b2eb3db8dcfec2621b01d13e50084528e2db6
Tim Jones <tdjones74021@yahoo.com>
2020-12-30 11:01:43 -0600

    Add handler for using 'q' to quit.

--- editor.py
+++ editor.py
@@ -3,10 +3,13 @@
 # https://wasimlorgat.com/editor
 
 import curses
+import sys
 
 def main(stdscr):
     while True:
         k = stdscr.getkey()
+        if k == "q":
+            sys.exit()
         
 if __name__ == "__main__":
     curses.wrapper(main)
